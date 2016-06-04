6f811046d053eedef1bcf9d7c9a1369fc7a1ebe3
Abraham Hokuto <shikieiki@yamaxandu.org>
2016-06-04 19:01:01 +0900

    enable optimizations by default

--- parser.c
+++ parser.c
@@ -1,5 +1,9 @@
 #include "parser.h"
 
+#define USE_STREQU5
+#define USE_BRANCH_PREDICT
+#define USE_CHAR_MAPS
+
 #if defined USE_STREQU5
 #define STREQU5(A, B) ((A[0] == B[0]) && (A[1] == B[1])		\
 		       && (A[2] == B[2]) && (A[3] == B[3])	\
